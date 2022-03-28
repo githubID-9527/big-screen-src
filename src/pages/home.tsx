95f5c235a4b8f2161fa3f3f054662aff32e6cd9b
haha <1732976075@qq.com>
2022-03-28 22:52:31 +0800

    划分中栏底部饼图位置

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,21 +2,43 @@ import React, { useEffect, useRef } from 'react';
 import './home.scss';
 import headBg from '../images/header.png';
 import { Chart1 } from '../components/chart-1';
-import {Chart2} from '../components/chart-2';
+import { Chart2 } from '../components/chart-2';
+import { Chart3 } from '../components/chart-3';
+import { Chart4 } from '../components/chart-4';
+import { Chart5 } from '../components/chart-5';
+import { Chart6 } from '../components/chart-6';
+import { Chart7 } from '../components/chart-7';
+import { Chart8 } from '../components/chart-8';
+import { Chart9 } from '../components/chart-9';
 
 const px = (n) => n / 2420 * (window as any).pageWidth;
 export const Home = () => {
   return (
     <div className='home'>
-      <header style={{backgroundImage: `url(${headBg})` }}></header>
+      <header style={{ backgroundImage: `url(${headBg})` }}></header>
       <main>
-      <section className="section1">
-          <Chart1/>
-          <Chart2/>
+        <section className="section1">
+          <Chart1 />
+          <Chart2 />
         </section>
-        <section className="bordered section2"></section>
-        <section className="bordered section3"></section>
-        <section className="bordered section4"></section>
+        <section className="bordered section2">
+          <Chart3 />
+          <Chart4 />
+        </section>
+        <section className="bordered section3">
+          <Chart5 />
+        </section>
+        <section className="bordered section4">
+          <Chart6 />
+          <div className="bordered 年龄段">
+            <h2>犯罪人员年龄段分布</h2>
+            <div className="charts">
+              <Chart7 />
+              <Chart8 />
+              <Chart9 />
+            </div>
+          </div>
+        </section>
         <section className="bordered section5"></section>
       </main>
       <footer>
