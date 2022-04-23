75c2ce6e8cc0ad4ed6f82c10d951218bae3bb5ec
haha <1732976075@qq.com>
2022-04-23 15:18:03 +0800

    完成 第三栏统计

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,8 +10,12 @@ import { Chart6 } from '../components/chart-6';
 import { Chart7 } from '../components/chart-7';
 import { Chart8 } from '../components/chart-8';
 import { Chart9 } from '../components/chart-9';
+import { Chart10 } from '../components/chart-10';
+import {Chart11} from '../components/chart-11';
+import {Chart12} from '../components/chart-12';
+import {Chart13} from '../components/chart-13';
+import {Chart14} from '../components/chart-14';
 
-const px = (n) => n / 2420 * (window as any).pageWidth;
 export const Home = () => {
   return (
     <div className='home'>
@@ -39,7 +43,26 @@ export const Home = () => {
             </div>
           </div>
         </section>
-        <section className="bordered section5"></section>
+        <section className="bordered section5">
+          <div className="bordered row1 案发类型">
+            <h2>案发类型统计</h2>
+            <div className="charts">
+              <Chart10/>
+              <Chart11/>
+            </div>
+          </div>
+          <div className="bordered row2 案发街道">
+            <h2>案发街道统计</h2>
+            <div className="charts">
+            <Chart12/>
+            <Chart13/>
+            </div>
+          </div>
+          <div className="bordered row3 作案手段">
+            <h2>作案手段分析</h2>
+            <Chart14/>
+          </div>
+        </section>
       </main>
       <footer>
         &copy; 2022
